1994b678ae2d7c6e888e775cfa48c23b56f978d3
Mộc Dương Lâm <mocduonglam86@gmail.com>
2022-01-30 14:31:16 +0700

    setup

--- VLU-CV/VLU-CV/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ VLU-CV/VLU-CV/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  selector: "app-nav-menu",
+  templateUrl: "./nav-menu.component.html",
+  styleUrls: ["./nav-menu.component.css"],
 })
 export class NavMenuComponent {
   isExpanded = false;
-
+  logoPath = "src/image/vlu-logo.png";
   collapse() {
     this.isExpanded = false;
   }
