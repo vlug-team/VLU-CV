605518ade89004117f2fb22dcbf89005999659ee
Mộc Dương Lâm <mocduonglam86@gmail.com>
2022-02-05 03:53:47 +0700

    start

---
+++ VLU-CV/VLU-CV/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VLU_CV.Models;
+
+namespace VLU_CV.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+        public DbSet<CurriculumVitae> CurriculumVitaes{ get; set; }
+    }
+}
---
+++ VLU-CV/VLU-CV/Models/CurriculumVitae.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLU_CV.Models
+{
+    public class CurriculumVitae
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar")]
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar")]
+        public string ImageAvatar { get; set; }
+        [Column(TypeName = "nvarchar")]
+        public string Introduce { get; set; }
+    }
+}
