6ddccfe3f177c298d6515acf715ae5743effddd7
Mộc Dương Lâm <mocduonglam86@gmail.com>
2022-02-08 02:58:34 +0700

    footer

---
+++ VLU-CV/VLU-CV/ClientApp/src/app/app-footer/app-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './app-footer.component.html',
+  styleUrls: ['./app-footer.component.css']
+})
+export class AppFooterComponent {
+
+
+}
