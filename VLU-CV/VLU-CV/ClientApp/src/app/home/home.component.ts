04876dfae0deec794dda29ebe841072387910feb
darkicecre <vophiminhhieuthukhoanghia@gmail.com>
2022-02-09 12:57:04 +0700

    Done

--- VLU-CV/VLU-CV/ClientApp/src/app/home/home.component.ts
+++ VLU-CV/VLU-CV/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  imageCVsource = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuOyp3fWuVg6dTdcXj6xOadAmVCLxFERCuiw&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuOyp3fWuVg6dTdcXj6xOadAmVCLxFERCuiw&usqp=CAU", 
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuOyp3fWuVg6dTdcXj6xOadAmVCLxFERCuiw&usqp=CAU"
+  ];
 }
