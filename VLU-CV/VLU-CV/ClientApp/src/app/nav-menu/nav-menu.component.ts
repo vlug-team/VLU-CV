64c796cba9a2fd42a126b94ff0c2c0c3db9855f1
Mộc Dương Lâm <mocduonglam86@gmail.com>
2022-03-07 03:24:24 +0700

    login

--- VLU-CV/VLU-CV/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ VLU-CV/VLU-CV/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  selector: "app-nav-menu",
+  templateUrl: "./nav-menu.component.html",
+  styleUrls: ["./nav-menu.component.css"],
 })
 export class NavMenuComponent {
   isExpanded = false;
+  user;
   collapse() {
     this.isExpanded = false;
   }
---
+++ VLU-CV/VLU-CV/Controllers/AccountCotroller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLU_CV.Controllers
+{
+    public class AccountCotroller
+    {
+    }
+}
