280fc4c1188bd2b9e70a3a9a0d37f74f3e074025
Mộc Dương Lâm <mocduonglam86@gmail.com>
2022-03-07 13:51:12 +0700

    update

--- VLU-CV/VLU-CV/ClientApp/src/app/app.module.ts
+++ VLU-CV/VLU-CV/ClientApp/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { NavMenuComponent } from "./nav-menu/nav-menu.component";
 import { AppFooterComponent } from "./app-footer/app-footer.component";
 import { HomeComponent } from "./home/home.component";
-import { CounterComponent } from "./counter/counter.component";
 import { ResumeComponent } from "./resume/resume.component";
-import { FetchDataComponent } from "./fetch-data/fetch-data.component";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AboutComponent } from "./about/about.component";
@@ -20,10 +18,8 @@ import { AboutComponent } from "./about/about.component";
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
     ResumeComponent,
     AboutComponent,
-    FetchDataComponent,
     AppFooterComponent,
   ],
   imports: [
@@ -32,10 +28,8 @@ import { AboutComponent } from "./about/about.component";
     FormsModule,
     RouterModule.forRoot([
       { path: "", component: HomeComponent, pathMatch: "full" },
-      { path: "counter", component: CounterComponent },
       { path: "about", component: AboutComponent },
       { path: "resume", component: ResumeComponent },
-      { path: "fetch-data", component: FetchDataComponent },
     ]),
     NgbModule,
   ],
