db9587f1c4dfdc93d10fdea49da448c794e62b23
Mộc Dương Lâm <mocduonglam86@gmail.com>
2022-03-28 10:25:28 +0700

    done

--- VLU-CV/VLU-CV/Models/CurriculumVitae.cs
+++ VLU-CV/VLU-CV/Models/CurriculumVitae.cs
@@ -11,27 +11,66 @@ namespace VLU_CV.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string FullName { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string Position { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string PersonalStatement { get; set; }
         public DateTime BirthDay { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Email { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string PhoneNumber { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string FacebookUrl { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Address { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Skill_1 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string SkillDecription_1 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Skill_2 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string SkillDecription_2 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Specialized { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string SchoolName { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public double GPA { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Positioned_1 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Worked_1 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Description_1 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Positioned_2 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Worked_2 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Description_2 { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
         public string Achievement { get; set; }
     }
 }
