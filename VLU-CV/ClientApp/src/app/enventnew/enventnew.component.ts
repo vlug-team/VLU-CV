16228260938a3395311038fc1d5ae7f3231d5078
morclaw <cong.207ct27643@vanlanguni.vn>
2022-04-14 06:31:14 +0700

    update

--- VLU-CV/ClientApp/src/app/enventnew/enventnew.component.ts
+++ VLU-CV/ClientApp/src/app/enventnew/enventnew.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-enventnew',
-  templateUrl: './enventnew.component.html',
-  styleUrls: ['./enventnew.component.css']
+	selector: 'app-enventnew',
+	templateUrl: './enventnew.component.html',
+	styleUrls: ['./enventnew.component.css']
 })
 export class EnventnewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	index: number = 0;
+	visible = true;
+	dataFake = [
+		{
+			id: 1,
+			image: '../../../assets/Listpage/firter.png',
+			title: 'Thông tin cuộc thi chinh phục CV, có ngay công việc - Career Catching',
+			date: '12/12/2019',
+			content: "Cách để CV của bạn được chuyên viên sửa chữa và được nhà tuyển dụng trực tiếp xem ..."
+		},
+		{
+			id: 2,
+			image: '../../../assets/Listpage/resume-event.jpg',
+			title: 'Các yếu tố giúp Resume của bạn trở nên ấn tượng hơn trong mắt nhà tuyển dụng',
+			date: '12/12/2019',
+			content: "Bật mí bí quyết trình bày CV gây ấn tượng với nhà tuyển dụng ..."
+		},
+		{
+			id: 3,
+			image: '../../../assets/Listpage/interviewing.jpg',
+			title: 'Các yếu tố giúp CV của bạn trở nên ấn tượng hơn trong mắt nhà tuyển dụng',
+			date: '12/12/2019',
+			content: "Bật mí bí quyết trình bày CV gây ấn tượng với nhà tuyển dụng ..."
+		},
+		{
+			id: 4,
+			image: '../../../assets/Listpage/mangxahoi.png',
+			title: 'Các yếu tố giúp CV của bạn trở nên ấn tượng hơn trong mắt nhà tuyển dụng',
+			date: '12/12/2019',
+			content: "Bật mí bí quyết trình bày CV gây ấn tượng với nhà tuyển dụng ..."
+		},
+		{
+			id: 5,
+			image: '../../../assets/Listpage/tranhkhiphongvan.jpg',
+			title: 'Các yếu tố giúp CV của bạn trở nên ấn tượng hơn trong mắt nhà tuyển dụng',
+			date: '12/12/2019',
+			content: "Bật mí bí quyết trình bày CV gây ấn tượng với nhà tuyển dụng ..."
+		},
+		{
+			id: 6,
+			image: '../../../assets/Listpage/vlupanel.png',
+			title: 'Các yếu tố giúp CV của bạn trở nên ấn tượng hơn trong mắt nhà tuyển dụng',
+			date: '12/12/2019',
+			content: "Bật mí bí quyết trình bày CV gây ấn tượng với nhà tuyển dụng ..."
+		},
+		{
+			id: 7,
+			image: '../../../assets/Listpage/vlupanel.png',
+			title: 'Các yếu tố giúp CV của bạn trở nên ấn tượng hơn trong mắt nhà tuyển dụng',
+			date: '12/12/2019',
+			content: "Bật mí bí quyết trình bày CV gây ấn tượng với nhà tuyển dụng ..."
+		},
+		{
+			id: 8,
+			image: '../../../assets/Listpage/vlujob.png',
+			title: 'Các yếu tố giúp CV của bạn trở nên ấn tượng hơn trong mắt nhà tuyển dụng',
+			date: '12/12/2019',
+			content: "Bật mí bí quyết trình bày CV gây ấn tượng với nhà tuyển dụng ..."
+		}
+	];
+	// var get element by id
+	ngOnInit(): void {
+	}
 }
+
