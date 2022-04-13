761fe0437dc7484335ac6fd23ad9a184e3ee4e35
morclaw <cong.207ct27643@vanlanguni.vn>
2022-04-13 19:18:40 +0700

    update gui

--- VLU-CV/ClientApp/src/app/profile/profile.component.ts
+++ VLU-CV/ClientApp/src/app/profile/profile.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { SocialAuthService, SocialUser } from 'angularx-social-login';
+import { Component, Inject, Injectable, OnInit } from '@angular/core';
 import jsPDF from 'jspdf';
-import { Observable, of } from 'rxjs';
 import { CreateCv } from 'src/shared/createcv.model';
 import { CreateCvService } from 'src/shared/createcv.service';
 import * as htmlToImage from 'html-to-image';
-
 @Component({
 	selector: 'app-profile',
 	templateUrl: './profile.component.html',
 	styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
 	constructor(public service: CreateCvService) { };
 	data: CreateCv[];
@@ -20,7 +18,7 @@ export class ProfileComponent implements OnInit {
 			console.log(this.data);
 		})
 	}
-	name() {
+	download() {
 		htmlToImage.toPng(document.getElementById('cv'))
 			.then(function (dataUrl) {
 
@@ -31,4 +29,6 @@ export class ProfileComponent implements OnInit {
 			});
 		console.log(this.data);
 	}
+
+
 }
